a59319172d7a7b50ea4c42aea028cf07d6fc6065
Suriya U <141810139+USuriya@users.noreply.github.com>
2025-12-15 19:22:45 +0530

    Add files via upload

---
+++ primitive_TC_1.java
@@ -0,0 +1,19 @@
+package PREP_PACKAGE;
+
+public class primitive_TC_1 {
+	
+	public static void main(String[] args) {
+		
+		int a = 20;
+		long b =a;
+		double c =b;
+		short d = (short)a;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
