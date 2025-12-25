aeb4c775c968e8fe9422d1dd9d4082c73d8c8324
Suriya U <141810139+USuriya@users.noreply.github.com>
2025-12-25 23:56:02 +0530

    Add files via upload

---
+++ array_1.java
@@ -0,0 +1,21 @@
+package arrays;
+import java.util.Arrays;
+
+public class array_1 {
+	
+	public static void main(String[] args) {
+		
+		int[] my_arr =new int[5];
+		my_arr[0]=100;
+		my_arr[1]=200;
+		my_arr[2]=100;
+		my_arr[3]=100;
+		my_arr[4]=100;
+		
+		String[] arr2 = new String[3];
+		double[] arr3 = new double[6];
+		
+		System.out.println(Arrays.toString(my_arr));
+	}
+
+}
